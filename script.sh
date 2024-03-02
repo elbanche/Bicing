8f177a7511429f24e35cfde6315e8f81eb3eb195
elbanche <elbanche@gmail.com>
2024-03-02 15:48:38 +0100

    update

--- script.sh
+++ script.sh
@@ -2,42 +2,42 @@
 
 echo "Ejecutando generate_station_csv.py"
 echo "Hora de inicio: $(date)"
-python ./data/generate_station_csv.py
+python3 ./data/generate_station_csv.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando resample_csv.py"
 echo "Hora de inicio: $(date)"
-python ./data/resample_csv.py
+python3 ./data/resample_csv.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando split_train_test.py"
 echo "Hora de inicio: $(date)"
-python ./data/split_train_test.py
+python3 ./data/split_train_test.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando model_dummy.py"
 echo "Hora de inicio: $(date)"
-python ./models/dummy/model_dummy.py
+python3 ./models/dummy/model_dummy.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando model_avg.py"
 echo "Hora de inicio: $(date)"
-python ./models/avg/model_avg.py
+python3 ./models/avg/model_avg.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando model_rnn.py"
 echo "Hora de inicio: $(date)"
-python ./models/rnn/model_rnn.py
+python3 ./models/rnn/model_rnn.py
 echo "Hora de finalización: $(date)"
 echo
 
 echo "Ejecutando model_rnn_by_time.py"
 echo "Hora de inicio: $(date)"
-python ./models/rnn_by_time/model_rnn_by_time.py
+python3 ./models/rnn_by_time/model_rnn_by_time.py
 echo "Hora de finalización: $(date)"
 echo
